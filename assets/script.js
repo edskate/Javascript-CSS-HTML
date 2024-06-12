eddc96477ad7b4606cc8e38c8f3a87e121316817
Edy_97_2024 <edskateicevida@gmail.com>
2024-06-12 17:08:07 +0000

    atualização

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,36 @@
 //tipos  de váriaveis:
 
-let idade = 90 
-let nome = "lu"
-let logado = false
+let idade = 18//tipo number
+let nome = "lu" //tipo string
+let logado = false // tipo boolean
 
 let ingredientes = ["arroz", "feijao", "macarrao", 'arroz']
 let carro = ['pejo', 'fusca', 'monza']
 let Make = ['po-compacto','Bluche', 'pincel', 'baton', 'corretivo']
+let nomes = ['julia','calral','antonio','luiz','bruna','carminha']
+let objetos = ['cadeira','sofa','garrafa','prato','panela','lapis','caderno','mochila']
+
+
+
+
 
 
 console.log(ingredientes[3]);
-console.log(carro)
-console.log(Make)
+console.log(carro[0])
+console.log(Make[2])
+console.log(nomes[3])
+console.log(objetos[4])//por nomes
+console.log(nomes)//lista
 
+
+
+if (idade >= 18) {
+    console.log('você e maior de idade')
+}
+
+
+if (nome == 'lu') {
+    console.log("mensagem especial")
+}else {
+    console.log("mensagem normal")
+}
