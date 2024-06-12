23151e971741211b2f2da7c96b998bd8fce69fff
Edy_97_2024 <edskateicevida@gmail.com>
2024-06-12 05:01:01 +0000

    variaveis

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
-
+//tipos  de váriaveis:
 
 let idade = 90 
 let nome = "lu"
 let logado = false
 
-let ingredientes = ["arroz", "feijao", "macarrao"]
+let ingredientes = ["arroz", "feijao", "macarrao", 'arroz']
+let carro = ['pejo', 'fusca', 'monza']
+let Make = ['po-compacto','Bluche', 'pincel', 'baton', 'corretivo']
 
-console.log(ingredientes[1]);
+
+console.log(ingredientes[3]);
+console.log(carro)
+console.log(Make)
+
